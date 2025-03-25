c64cee8a510826db992556b1a891f8ff2f343b08
Andhika Nusantara <dikanusantara00@gmail.com>
2025-03-25 10:07:35 +0700

    add java enum Platform

---
+++ src/test/java/com/projectname/keywords/utils/Platform.java
@@ -0,0 +1,11 @@
+package com.projectname.keywords.utils;
+
+public enum Platform {
+    CHROME,
+    CHROME_HEADLESS,
+    FIREFOX,
+    FIREFOX_HEADLESS,
+    EDGE,
+    EDGE_HEADLESS,
+    SAFARI
+}
