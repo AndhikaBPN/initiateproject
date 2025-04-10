de19d3f1f4500cad3c4a4b97376c55a46a162f14
Andhika Nusantara <dikanusantara00@gmail.com>
2025-04-10 10:59:10 +0700

    add listener to send email

--- src/test/java/com/projectname/testlistener/Listener.java
+++ src/test/java/com/projectname/testlistener/Listener.java
@@ -1,5 +1,35 @@
 package com.projectname.testlistener;
 
-public class Listener {
+import com.projectname.keywords.utils.ConfigReader;
+import com.projectname.keywords.utils.EmailReportSender;
+import com.projectname.keywords.utils.TestLogger;
+import com.projectname.keywords.utils.Utils;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+
+public class Listener implements ITestListener {
+
+    boolean sendEmail = ConfigReader.getBooleanProperty("sendEmail");
+    String projectName = ConfigReader.getProperty("projectName");
+    String dateTime = Utils.generateDateTime();
+
+    @Override
+    public void onFinish(ITestContext context) {
+        String project = projectName.toLowerCase().replace(" ", "-");
+        TestLogger.logInfo("Send Email: " + sendEmail);
+
+        if(!sendEmail) {
+            System.out.println("All tests finished. Email not Sent");
+            return;
+        }
+
+        System.out.println("All tests finished. Sending test report...");
+
+        // Generate test report
+        Utils.generateReport(project, dateTime);
+
+        // Send email with report
+        EmailReportSender.sendReport(project, dateTime);
+    }
 
 }
